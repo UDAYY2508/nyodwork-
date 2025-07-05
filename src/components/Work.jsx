74ee60826e27e556b980d87d0bbde25f7ea851a0
uday <udayghorpade2508@gmail.com>
2025-07-05 09:44:27 +0530

    fix: update workImage1 import to use the latest screenshot

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -1,4 +1,4 @@
-import workImage1 from '../assets/Screenshot 2025-07-03 092153.png';
+import workImage1 from '../assets/Screenshot 2025-07-05 094024.png';
 import workImage2 from '../assets/Screenshot 2025-02-14 170557.png';
 import workImage3 from '../assets/Screenshot 2025-03-15 194329.png';
 import workImage4 from '../assets/Screenshot 2025-07-04 153359.png';
